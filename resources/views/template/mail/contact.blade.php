dc122f5e22acfabd240d45470bfc0e10ea274a61
Breno Rodrigues <alugueimovelsc@gmail.com>
2018-06-17 15:22:30 -0300

    Iniatial

--- resources/views/template/mail/contact.blade.php
+++ resources/views/template/mail/contact.blade.php
@@ -1,6 +1,6 @@
 <h1>Contato via formulário</h1>
 
-<p>Nome: <b>{{ $request->nome }}</b></p>
-<p>Email: <b>{{ $request->email }}</b></p>
-<p>Telefone: <b>{{ $request->telefone }}</b></p>
-<p>Bairro: <b>{{ $request->bairro }}</b></p>
+<p>Nome: <b>{{ $request['nome'] }}</b></p>
+<p>Email: <b>{{ $request['email'] }}</b></p>
+<p>Telefone: <b>{{ $request['telefone'] }}</b></p>
+<p>Bairro: <b>{{ $request['bairro'] }}</b></p>
